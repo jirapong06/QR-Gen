881c39fe247b5a58207097e173fa352f998d4d02
jira <jirapong06@gmail.com>
2023-03-07 14:17:56 +0700

    page

---
+++ app.js
@@ -0,0 +1,27 @@
+var qrcode = new QRCode(document.getElementById("qrcode"), {
+    width : 200,
+    height : 200
+});
+
+function makeCode () {		
+     elText = document.getElementById("text");
+
+    if (!elText.value) {
+        alert("กรุณากรอก URL");
+        elText.focus();
+        return;
+    }
+
+    if (elText.value.startsWith("https://")) {
+    } else if (elText.value.startsWith("http://")){
+    } else {
+        elText.value = "https://"+elText.value
+    }
+
+    qrcode.makeCode(elText.value);
+}
+
+$("#genBtn").click(function() {
+    makeCode();
+    $("#qrcode").attr("display","block")
+})
